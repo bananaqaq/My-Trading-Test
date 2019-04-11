b232e68211f6950d8b6cb889a23d9451a2b9ec04
zhuang_cong <zhuangc@disanbo.com>
2019-04-11 18:02:48 +0800

    update

--- src/main/java/newbbb/service/IAccountService.java
+++ src/main/java/newbbb/service/IAccountService.java
@@ -1,8 +1,5 @@
 package newbbb.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface IAccountService {
 
 }
--- src/main/java/newbbb/service/impl/AccountServiceImpl.java
+++ src/main/java/newbbb/service/impl/AccountServiceImpl.java
@@ -1,6 +1,8 @@
 package newbbb.service.impl;
 
 import newbbb.service.IAccountService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AccountServiceImpl implements IAccountService {
 }
