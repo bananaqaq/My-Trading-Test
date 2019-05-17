8628031d1f4ea07018f82e002be2bb361f961511
zhuang_cong <zhuangc@disanbo.com>
2019-05-17 17:55:26 +0800

    update

--- src/main/java/newbbb/matchengine/MEMain.java
+++ src/main/java/newbbb/matchengine/MEMain.java
@@ -1,11 +1,22 @@
 package newbbb.matchengine;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MEMain {
 
+    private static ApplicationContext context;
 
+    static {
+        context = new ClassPathXmlApplicationContext("classpath:spring/application-context.xml");
+    }
 
-    private int init(){
-        return 0;
+    public static  void main(String[] args){
+
+        Logger log = LoggerFactory.getLogger(MEMain.class);
+
     }
 
 }
